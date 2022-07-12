2fa9b319cd3d27aab094118ae391cf7d30960aa9
sudongyu <976499226@qq.com>
2022-07-13 01:28:09 +0800

    chore: update App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,39 +2,18 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is12311: {count}
-          </button>
+          Sudongyuer Demo show 🐟
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          vite-plugin-hot-export
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        {/* <img src={Gold}></img> */}
       </header>
     </div>
   )
