24f90e7c26ea57846e64e9cb0f716da54303046a
sudongyu <976499226@qq.com>
2022-07-13 23:18:27 +0800

    fix: vite build process not exit

--- packages/vite-plugin-hot-export/src/index.ts
+++ packages/vite-plugin-hot-export/src/index.ts
@@ -6,32 +6,38 @@ import { loadConfig } from 'unconfig'
 import chokidar from 'chokidar'
 
 function VitePluginHotExport(): Plugin {
-  loadConfig<ExportConfig>({
-    sources: [
-      {
-        files: 'export.config',
-        extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json', ''],
-      },
-    ],
-  }).then(({ config: configArray }) => {
-    const configs = configArray.configs
-    configs.forEach((config) => {
-      const { targetDir } = config
-      chokidar.watch(path.resolve(cwd(), config.targetDir)).on('all', (event, pathDir) => {
-        if (pathDir !== path.resolve(cwd(), targetDir, 'index.ts')) {
-          try {
-            child_process.execSync('pnpm autoexport')
-          }
-          catch (error) {
-            child_process.execSync('npx autoexport')
-          }
-        }
-      })
-    })
-  })
-
   return {
     name: 'vite-plugin-hot-export',
+    apply: 'serve',
+    config(config, { command }) {
+      if (command === 'build')
+        console.warn('build!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+      if (command === 'serve') {
+        loadConfig<ExportConfig>({
+          sources: [
+            {
+              files: 'export.config',
+              extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json', ''],
+            },
+          ],
+        }).then(({ config: configArray }) => {
+          const configs = configArray.configs
+          configs.forEach((config) => {
+            const { targetDir } = config
+            chokidar.watch(path.resolve(cwd(), config.targetDir)).on('all', (event, pathDir) => {
+              if (pathDir !== path.resolve(cwd(), targetDir, 'index.ts')) {
+                try {
+                  child_process.execSync('pnpm autoexport')
+                }
+                catch (error) {
+                  child_process.execSync('npx autoexport')
+                }
+              }
+            })
+          })
+        })
+      }
+    },
   }
 }
 
