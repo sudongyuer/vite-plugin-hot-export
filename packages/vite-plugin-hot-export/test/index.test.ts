472fabad5a55cfe90548c2dae7babb54f4fd3335
Frozen FIsh <976499226@qq.com>
2022-07-17 01:21:44 +0800

    feat: add test

--- packages/vite-plugin-hot-export/test/index.test.ts
+++ packages/vite-plugin-hot-export/test/index.test.ts
@@ -1,7 +1,29 @@
+import path from 'node:path'
+import { cwd } from 'node:process'
+import fs from 'fs'
 import { describe, expect, it } from 'vitest'
-
 describe('should', () => {
   it('exported', () => {
     expect(1).toEqual(1)
   })
+})
+
+it('getCurrentDirFilesCount', () => {
+  function getCurrentDirFilesCount(targetDir: string) {
+    let filesLength = 0
+    const DirAndFiles = fs.readdirSync(path.resolve(cwd(), targetDir))
+    for (let i = 0; i < DirAndFiles.length; i++) {
+      const file = DirAndFiles[i]
+      const filePath = path.resolve(cwd(), targetDir, file)
+      const stats = fs.statSync(filePath)
+      if (stats.isFile())
+        filesLength++
+      else
+        filesLength += getCurrentDirFilesCount(filePath)
+    }
+    return filesLength
+  }
+
+  const length = getCurrentDirFilesCount(path.resolve(cwd(), './'))
+  expect(length).toBe(4)
 })
