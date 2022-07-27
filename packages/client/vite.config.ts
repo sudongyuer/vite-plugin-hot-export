7d247f13b4c6eba74bbb2f6cd9b27183c851ed29
Frozen FIsh <976499226@qq.com>
2022-07-27 13:10:49 +0800

    chore: update

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import Inspect from 'vite-plugin-inspect'
 import HotExport from 'vite-plugin-hot-export'
 import svgr from 'vite-plugin-svgr'
+import ReactInspector from 'vite-plugin-react-inspector'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [HotExport(), Inspect(), react(), svgr({
+  plugins: [ReactInspector(), HotExport(), Inspect(), react(), svgr({
     svgrOptions: {
       icon: true,
       // ...svgr options (https://react-svgr.com/docs/options/)
